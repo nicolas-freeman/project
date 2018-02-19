2af6ed4f930ccb5a4e1741d4e1b3f371a6e6a8f2
Nicolas Freeman <nicolas.d.freeman@gmail.com>
2018-02-19 17:58:38 +0100

    Améliorations mineures
    Principalement page entreprises et page login

--- js/code.js
+++ js/code.js
@@ -31,9 +31,12 @@ function onSignin(element) {
     console.log(element.id);
     auth2.attachClickHandler(element, {},
         function (googleUser) {
+            // Idée d'ajout : cacher l'alerte de déconnexion si elle était affichée
             document.getElementById('name').innerText = "Connecté en tant que " +
                 googleUser.getBasicProfile().getName();
             console.log("onSignIn");
+
+            // Récupération des infos de l'utilisateur
             var profile = googleUser.getBasicProfile();
             console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
             user.ID = profile.getId();
@@ -43,12 +46,26 @@ function onSignin(element) {
             user.photoURL = profile.getImageUrl();
             console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
             user.email = profile.getEmail();
-            // Store the entity object in sessionStorage where it will be accessible from all pages of the site.
+
+            // Stockage des infos dans sessionStorage, qui sera accessible depuis n'importe quelle page du site
             sessionStorage.setItem('user', JSON.stringify(user));
+
+            // Mise à jour de la barre de navigation pour prendre en compte la connexion
+            log.innerHTML = log.innerHTML.replace("Connexion", "Déconnexion"); // On modifie le texte du bouton
+            log.href = "login.html"; // On modifie le lien du bouton | To update
+            document.getElementById('profile-img').src = user.photoURL; // On remplace l'image de profil de la page
+            document.getElementsByClassName('nav-profile-img')[0].src = user.photoURL; // On remplace l'image de profil de la barre de navigation
+            $("#logoutButton").toggleClass("collapse collapse.show"); // On affiche le bouton de déconnexion
+            console.log ("Bouton de déconnexion affiché");
+
+            if (sessionStorage.getItem('user') !== null && user.email.split("@").pop() !== "essec.edu") { // Si connecté avec un compte non ESSEC
+                $("#notESSECAlert").toggleClass("collapse collapse.show"); // On affiche l'alerte demandant la connexion avec un compte ESSEC
+                console.log ("Alerte de connexion avec un compte non ESSEC affichée");
+            }
+
             // Redirection vers l'accueil du site
             // window.location.href = 'index.html';
-            document.getElementById('profile-img').src = user.photoURL; // On remplace l'image de profil de la page
-            document.getElementById('nav-profile-img').src = user.photoURL; // On remplace l'image de profil de la barre de navigation
+
             console.log("endOfSignIn");
         }, function (error) {
             alert(JSON.stringify(error, undefined, 2));
@@ -58,33 +75,15 @@ function onSignin(element) {
 
 // FONCTIONS
 
-
-function onSignIn(googleUser) {
-    console.log("onSignIn");
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    user.ID = profile.getId();
-    console.log('Name: ' + profile.getName());
-    user.name = profile.getName();
-    console.log('Image URL: ' + profile.getImageUrl());
-    user.photoURL = profile.getImageUrl();
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    user.email = profile.getEmail();
-    // Store the entity object in sessionStorage where it will be accessible from all pages of the site.
-    sessionStorage.setItem('user', JSON.stringify(user));
-    // Redirection vers l'accueil du site
-    window.location.href = 'index.html';
-    console.log("endOnSignIn");
-}
-
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
     });
     sessionStorage.removeItem('user')
-    // Redirection vers l'accueil du site
-    window.location.href = 'index.html';
+    sessionStorage.setItem('logOut', true);
+    // Redirection vers la page de connexion
+    window.location.href = 'login.html';
 }
 
 function init() {
@@ -105,35 +104,41 @@ function checkIfLoggedIn() {
     } else if (sessionStorage.getItem('user') !== null && user.email.split("@").pop() == "essec.edu") { // Si connecté avec un compte ESSEC
         // On ne fait rien
     } else if (sessionStorage.getItem('user') !== null && user.email.split("@").pop() !== "essec.edu") { // Si connecté avec un compte non ESSEC
-        console.log("Merci de vous connecter avec un compte ESSEC");
+        $("#notESSECAlert").toggleClass("collapse collapse.show"); // On affiche l'alerte demandant la connexion avec un compte ESSEC
         // Redirection vers la page de connexion
         window.location.href = 'login.html';
     }
     else { // Sinon
-        
+
     }
 }
 
-// Une fois la page chargée, si connecté, on remplace connexion par déconnexion (texte et lien) dans la barre de navigation, et vice-versa
+// Une fois la page chargée...
 window.onload = function () {
     if (sessionStorage.getItem('user') == null) { // Si non connecté
         var log = document.getElementById("log");
-        console.log("Not logged in");
         log.innerHTML = log.innerHTML.replace("Déconnexion", "Connexion"); // On modifie le texte du bouton
         log.href = "login.html"; // On modifie le lien du bouton
+        if (sessionStorage.logOut == "true" && window.location.href.indexOf('login.html') >= 1) { // Si on vient de se déconnecter et sur la page de login
+            $("#loggedOutAlert").toggleClass("collapse collapse.show"); // On affiche l'alerte de déconnexion
+            sessionStorage.setItem('logOut', false); // "On ne vient plus de se déconnecter"
+        }
     }
     else { // Sinon
         if (window.location.href.indexOf('login.html') >= 1) { // Si connecté et sur la page de login
             document.getElementById('profile-img').src = user.photoURL; // On remplace l'image de profil de la page
             document.getElementById('name').innerText = "Connecté en tant que " + user.name;
+            $("#logoutButton").toggleClass("collapse collapse.show"); // On affiche le bouton de déconnexion
+            if (sessionStorage.getItem('user') !== null && user.email.split("@").pop() !== "essec.edu") { // Si connecté avec un compte non ESSEC
+                $("#notESSECAlert").toggleClass("collapse collapse.show"); // On affiche l'alerte demandant la connexion avec un compte ESSEC
+            }
         }
         var log = document.getElementById("log");
-        console.log("Logged in");
         log.innerHTML = log.innerHTML.replace("Connexion", "Déconnexion"); // On modifie le texte du bouton
-        log.href = "logout.html"; // On modifie le lien du bouton
-        var link = document.getElementById('nav-profile-img');
+        log.href = "login.html"; // On modifie le lien du bouton | To update
+        var link = document.getElementsByClassName('nav-profile-img')[0];
         console.log(link);
-        document.getElementById('nav-profile-img').src = user.photoURL; // On remplace l'image de profil de la barre de navigation
+        document.getElementsByClassName('nav-profile-img')[0].src = user.photoURL; // On remplace l'image de profil de la barre de navigation
     }
 }
 
@@ -150,10 +155,9 @@ function imgError(image) {
 $(document).ready(function () {
     $("#myInput").on("keyup", function () {
         var value = $(this).val().toLowerCase();
-        console.log("Recherche : "+value);
+        console.log("Recherche : " + value);
         $(".card").filter(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
 });
-

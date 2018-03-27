3b05464cf358ba235644bad4fffba9218110c3c7
Nicolas Freeman <nicolas.d.freeman@gmail.com>
2018-03-27 18:44:58 +0200

    Configuration pour serveur en ligne

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/sanslanguedeboite';
+var url = 'mongodb://localhost:22018/sanslanguedeboite';
 
 // Use connect method to connect to the Server
 
